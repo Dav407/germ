d3775b9db3a0e0b4fb62acdde1abcde61286e699
etim david <etimdavid407@gmail.com>
2024-07-11 07:24:39 -0700

    dataBase connected

--- api/index.js
+++ api/index.js
@@ -1,7 +1,17 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv'
 const port = 3000
-const app = express();
 
-app.listen(port, () => {
+dotenv.config();
+
+mongoose.connect(process.env.MONGO).then(() => {
+    console.log("MongoDB Connected")
+}).catch((err) => {
+    console.log(err);
+});
+
+const app = express();
+app.listen(3000, () => {
     console.log('listening on port ' + port)
 });
