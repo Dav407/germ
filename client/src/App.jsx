fdac48dafc27dac055873b11e9b74e20eb8473b1
etim david <etimdavid407@gmail.com>
2024-07-11 04:35:40 -0700

    pages and routes added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,21 @@
-
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import Dashboard from './pages/Dashboard'
+import Signup from './pages/Signup'
+import Signin from './pages/Signin'
+import Projects from './pages/Projects'
 export default function App() {
   return (
-    <div className="text-3xl text-red-500" >App</div>
+    <BrowserRouter  >
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/dashboard' element={<Dashboard />} />
+        <Route path='/projects' element={<Projects />} />
+        <Route path='/signin' element={<Signin />} />
+        <Route path='/signup' element={<Signup />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
