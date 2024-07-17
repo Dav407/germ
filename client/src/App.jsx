8ad5f234eb92a8253668188786a1a46a68f23197
etim david <etimdavid407@gmail.com>
2024-07-17 11:16:53 -0700

    Footer Component Added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,6 +6,7 @@ import Signup from './pages/Signup'
 import Signin from './pages/Signin'
 import Projects from './pages/Projects'
 import Header from './components/Header'
+import Footer from './components/Footer'
 export default function App() {
   return (
     <BrowserRouter  >
@@ -18,6 +19,7 @@ export default function App() {
         <Route path='/signin' element={<Signin />} />
         <Route path='/signup' element={<Signup />} />
       </Routes>
+      <Footer />
     </BrowserRouter>
   )
 }
