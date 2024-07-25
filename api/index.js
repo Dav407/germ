e055cc3bddd29ebcca38026e5d55b323c0b73677
etim david <etimdavid407@gmail.com>
2024-07-25 06:58:06 -0700

    update updateUser api route

--- api/index.js
+++ api/index.js
@@ -3,10 +3,11 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv'
 import userRoutes from './routes/user.route.js'
 import authRoutes from './routes/auth.route.js'
+import cookieParser from 'cookie-parser';
 const port = 3000
 
-dotenv.config(); 
-
+dotenv.config();  
+ 
 mongoose.connect(process.env.MONGO).then(() => {
     console.log("MongoDB Connected")
 }).catch((err) => {
@@ -15,8 +16,8 @@ mongoose.connect(process.env.MONGO).then(() => {
 
 const app = express();
 
-app.use(express.json())
-
+app.use(express.json());
+app.use(cookieParser());
 app.listen(3000, () => {
     console.log('listening on port ' + port)
 });
