71927f59cdb273f565eaf19b96f99adccec5bfe8
etim david <etimdavid407@gmail.com>
2024-08-02 10:22:05 -0700

    update functionality

--- client/src/pages/UpdatePost.jsx
+++ client/src/pages/UpdatePost.jsx
@@ -8,19 +8,43 @@ import {
   uploadBytesResumable,
 } from 'firebase/storage';
 import { app } from '../Firebase';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { CircularProgressbar } from 'react-circular-progressbar';
 import 'react-circular-progressbar/dist/styles.css';
-import { useNavigate } from 'react-router-dom';
-
-export default function CreatePost() {
+import { useNavigate, useParams } from 'react-router-dom';
+import { useSelector } from 'react-redux'
+ 
+export default function UpdatePost() {
   const [file, setFile] = useState(null);
   const [imageUploadProgress, setImageUploadProgress] = useState(null);
   const [imageUploadError, setImageUploadError] = useState(null);
   const [formData, setFormData] = useState({});
   const [publishError, setPublishError] = useState(null);
+  const { postId } = useParams();
 
   const navigate = useNavigate();
+  const { currentUser }  = useSelector((state) => state.user)
+
+  useEffect(() => {
+    try {
+        const fetchPost = async () => {
+            const res = await fetch(`/api/post/getposts?postId=${postId}`)
+            const data = await res.json();
+            if (!res.ok) {
+            console.log(data.message);
+            setPublishError(data.message);
+                return
+            }
+            if (res.ok) {
+                setPublishError(null)
+            setFormData(data.posts[0]);
+            }
+        }
+        fetchPost();
+    } catch (error) {
+        console.log(error);
+    }
+  }, [postId]);
 
   const handleUpdloadImage = async () => {
     try {
@@ -61,8 +85,8 @@ export default function CreatePost() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const res = await fetch('/api/post/create', {
-        method: 'POST',
+      const res = await fetch(`/api/post/updatePost/${formData._id}/${currentUser._id}`, {
+        method: 'PUT',
         headers: {
           'Content-Type': 'application/json',
         },
@@ -84,7 +108,7 @@ export default function CreatePost() {
   };
   return (
     <div className='p-3 max-w-3xl mx-auto min-h-screen'>
-      <h1 className='text-center text-3xl my-7 font-semibold'>Create a post</h1>
+      <h1 className='text-center text-3xl my-7 font-semibold'>Update post</h1>
       <form className='flex flex-col gap-4' onSubmit={handleSubmit}>
         <div className='flex flex-col gap-4 sm:flex-row justify-between'>
           <TextInput
@@ -96,11 +120,13 @@ export default function CreatePost() {
             onChange={(e) =>
               setFormData({ ...formData, title: e.target.value })
             }
+            value={formData.title}
           />
           <Select
             onChange={(e) =>
               setFormData({ ...formData, category: e.target.value })
             }
+            value={formData.category}
           >
             <option value='uncategorized'>Select a category</option>
             <option value='javascript'>JavaScript</option>
@@ -144,6 +170,7 @@ export default function CreatePost() {
         )}
         <ReactQuill
           theme='snow'
+          value={formData.content}
           placeholder='Write something...'
           className='h-72 mb-12'
           required
@@ -152,7 +179,7 @@ export default function CreatePost() {
           }}
         />
         <Button type='submit' gradientDuoTone='purpleToPink'>
-          Publish
+          Update Post
         </Button>
         {publishError && (
           <Alert className='mt-5' color='failure'>
