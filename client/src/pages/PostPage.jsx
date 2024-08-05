e6b53605b0840c90010bf16d3179781a90d9f8ee
etim david <etimdavid407@gmail.com>
2024-08-05 10:34:02 -0700

    add recent article section

--- client/src/pages/PostPage.jsx
+++ client/src/pages/PostPage.jsx
@@ -3,11 +3,13 @@ import React, { useEffect, useState } from 'react'
 import { Link, useParams } from 'react-router-dom'
 import CallToAction from '../components/CallToAction';
 import CommentSection from '../components/CommentSection';
+import PostCard from '../components/PostCard';
 export default function PostPage() {
   const { postSlug } = useParams();
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(false);
   const [post, setPost] = useState(null);
+  const [recentPosts, setRecentPosts] = useState(null);
 
   useEffect(() => {
     const fetchPost = async () => {
@@ -32,6 +34,25 @@ export default function PostPage() {
     };
     fetchPost();
   }, [postSlug]);
+
+  useEffect(() => {
+    try {
+      const fetchRecentPost = async () => {
+        const res = await fetch(`/api/post/getposts?limit=3`);
+        const data = await res.json();
+        if (!res.ok) {
+          console.log(data.message);
+          return;
+        }
+        if (res.ok) {
+          setRecentPosts(data.posts);
+        }
+      };
+      fetchRecentPost();
+    } catch (error) {
+      console.log(error.message);
+    }
+  }, []);
 
   if (loading) return (
     <div className="flex justify-center items-center min-h-screen">
@@ -63,7 +84,18 @@ export default function PostPage() {
         <CallToAction />
       </div>
        <CommentSection postId={post._id}/>
+
+       <div className="flex flex-col justify-center items-center mb-5">
+        <h1 className='text-xl mt-5'>Recent article</h1>
+        <div className="flex flex-wrap gap-5 mt-5 justify-center ">
+          {
+            recentPosts &&
+            recentPosts.map((post) => (
+              <PostCard key={post._id} post={post} />
+            ))
+          }
+        </div>
+       </div>
     </main>
   ) 
 }
-// 7:59:15
