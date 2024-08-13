8888ad81f7e27016bac91431e501e971751ed8f6
etim david <etimdavid407@gmail.com>
2024-08-13 10:50:42 -0700

    Home and Dash ui and functionalities done

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -5,6 +5,7 @@ import DashProfile from '../components/DashProfile';
 import Dashpost from '../components/Dashpost';
 import DashUsers from '../components/DashUsers';
 import DashComments from '../components/Dashcomment';
+import DashboardComponent from '../components/DashboardComponent';
 
 export default function Dashboard() {
   const location = useLocation();
@@ -31,6 +32,8 @@ export default function Dashboard() {
       {tab === 'users' && <DashUsers/> }
       {/* comments... */}
       {tab === 'comments' && <DashComments/> }
+      {/* dashboardComp... */}
+      {tab === 'dash' && <DashboardComponent/> }
     </div> 
   )
 }
