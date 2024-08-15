a53ae3a5c488ebd632e01ebff83c61aa5934ce0d
etim david <etimdavid407@gmail.com>
2024-08-14 22:06:13 -0700

    complete about and  project pages.

--- client/src/pages/Projects.jsx
+++ client/src/pages/Projects.jsx
@@ -1,7 +1,14 @@
-import React from 'react'
 
+
+
+import React from 'react'
+import CallToAction from '../components/CallToAction'
 export default function Projects() {
   return (
-    <div>Projects</div>
+    <div className='min-h-screen max-w-2xl mx-auto flex-col justify-center items-center p-3 gap-6'>
+      <h1 className='text-3xl font-semibold'>Projects</h1>
+      <p className='text-md text-gray-500'>Here you'll find a variety of articles and tutorials on topics such as web development, software engineering, and programming languages.</p>
+      <CallToAction />
+    </div>
   )
 }
